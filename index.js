e30e49d5da1d85b1f54afd3b40701c6062e430b5
Ivan Stoyanov <ivanstoyanov@outlook.com>
2024-02-28 15:30:55 +0100

    Lab solved

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister = `${s1} ${s2} ${s3} ${s4}, ${s5} ${s3} ${s2} ${s1} ${s4}.`;
 
 // Print out the concatenated string
 
-
-
+console.log(tongueTwister);
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -23,12 +22,14 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
-
 // Print the cameLtaiL-formatted string
+console.log(
+  `${part1.slice(0, 3)}${part1.at(-1).toUpperCase()}${part2.slice(0, 5)}${part2
+    .at(-1)
+    .toUpperCase()}`
+);
 
-
-
-
+console.log(`first`);
 /*******************************************
     Iteration 2.1 | Calculate Tip
 *******************************************/
@@ -36,23 +37,16 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
-
 // Print out the tipAmount
-
-
-
-
+console.log(billTotal * 0.15);
 /*******************************************
     Iteration 2.2 | Generate Random Number
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
 
-
 // Print the generated random number
-
-
-
+console.log(Math.floor(Math.random() * 10));
 /*******************************************
     Iteration 3.1 | Booleans
 *******************************************/
@@ -62,15 +56,24 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+//false
 const expression2 = a || b;
-
+//true
 const expression3 = !a && b;
-
+//false
 const expression4 = !(a && b);
-
+//true
 const expression5 = !a || !b;
-
+//true
 const expression6 = !(a || b);
-
+//false
 const expression7 = a && a;
+//true
+
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
+console.log(expression4);
+console.log(expression5);
+console.log(expression6);
+console.log(expression7);
